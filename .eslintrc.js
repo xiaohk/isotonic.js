598e83cc9a22aba86d24e73dd1d4a295ec24d845
Jay Wong <xiao.hk1997@gmail.com>
2021-06-05 15:04:38 -0400

    Init commit

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@shopify/esnext',
+  ],
+  rules: {
+    indent: [
+      'error',
+      2,
+    ],
+    'linebreak-style': [
+      'error',
+      'unix',
+    ],
+    quotes: [
+      'error',
+      'single',
+    ],
+    semi: [
+      'error',
+      'always',
+    ],
+  },
+  root: true,
+  globals: {
+    i32: 'readonly',
+  },
+};
