0283f6180f0e563f5cdcfa364ab6cb51e561c14b
Jay Wong <xiao.hk1997@gmail.com>
2021-06-13 17:18:29 -0400

    Use rollup to bundle mjs code and resolve node-module

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,18 @@
-import { wasm } from '@rollup/plugin-wasm';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 
-export default {
-  input: 'src/index.js',
+export default [{
+  input: 'src/index-cjs.js',
   output: {
     file: 'dist/cjs/index.js',
     format: 'cjs'
   },
-  plugins: [wasm()],
-};
+  plugins: [],
+},
+{
+  input: 'src/index-mjs.js',
+  output: {
+    file: 'dist/mjs/index.js',
+    format: 'es'
+  },
+  plugins: [nodeResolve()],
+}];
